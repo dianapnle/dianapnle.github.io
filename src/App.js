ad684dc61554a993bc2d048d654ebf0fcfef8473
Diana Le <dianapnle@gmail.com>
2024-08-24 16:07:38 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+
+import Home from './pages/Home'
+import Skill from './pages/Skillset'
+import Project from './pages/Projects'
+import Resume from './pages/Resume'
+import Contact from './pages/Contact'
+
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Preloader from "./components/PreLoader"
+import ScrollToTop from "./components/ScrollToTop"
+
+import "./App.css";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
+  const [load, upadateLoad] = useState(true);
 
-      </header>
-    </div>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/skillset" element={<Skill />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
