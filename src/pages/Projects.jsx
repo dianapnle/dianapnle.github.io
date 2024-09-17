39696db96080fb33d7be81c88458b86f84994140
Diana Le <dianapnle@gmail.com>
2024-09-16 22:28:52 -0700

    updated resume

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -25,6 +25,7 @@ const Projects = () => {
               title="UrbanSteps"
               description="Urban Steps is a website and online directory that helps people discover and get involved in local dance studios in their area. Urban Steps' users can view dance studios, instructors,classes, and write reviews. Studio owners can add instructors to their studios and be able to have class schedules on here too! The backend of Spirited Get Away is built on SQL, sequelize, and express with a PostgreSQL database. Frontend rendering is handled with React and Redux."
               ghLink="https://github.com/dianapnle/UrbanSteps"
+              demoLink="https://urbansteps.onrender.com/"
             />
           </Col>
 
@@ -35,6 +36,7 @@ const Projects = () => {
               title="Spirited Away"
               description="Spirited Get Away is a clone of the website Airbnb, an online market place that connects people who are looking for accomodations in a particular area with people who are renting out their properties. The backend of Spirited Get Away is built on SQL, sequelize, and express with a PostgreSQL database. Frontend rendering is handled with React and Redux."
               ghLink="https://github.com/dianapnle/spirited-away"
+              demoLink="https://spirited-away.onrender.com/"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -44,6 +46,7 @@ const Projects = () => {
               title="X-Talk"
               description="Cross-Talk is a clone of the website Discord, a free app that allows users to communicate in real time using text. Similarly, Cross-Talk allows for users to organize servers and smaller communities, rather than having a single central community. Users can join any public server and also create a smaller server for a group of friends.The backend of Cross-Talk is built on SQLAlchemy, Flask, python with a PostgreSQL database, AWS to handle media, and SocketIO for real-time messaging.  Frontend rendering is handled with React and Redux."
               ghLink="https://github.com/apatel978/Discord-Clone-XTalk"
+              demoLink="https://xtalk-h48u.onrender.com/"
             />
           </Col>
         </Row>
