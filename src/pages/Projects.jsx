2f3952aa7ee24a56ccf098e463f791537d64ab7c
Diana Le <dianapnle@gmail.com>
2024-09-21 21:12:59 -0700

    changes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -23,7 +23,7 @@ const Projects = () => {
               imgPath={urbansteps}
               isBlog={false}
               title="UrbanSteps"
-              description="Urban Steps is a website and online directory that helps people discover and get involved in local dance studios in their area. A lot of dance studios in my area currently do not have their own websites, so this project's purpose was to resolve basic interactions a dance studio and student would want."
+              description="Urban Steps is a website and online directory that helps people discover and get involved in local dance studios in their area. I have always been interested in combining my passion for dance and technical skills so I built this project to combine the two! The purpose was to resolve basic interactions a dance studio and student would want."
               ghLink="https://github.com/dianapnle/UrbanSteps"
               demoLink="https://urbansteps.onrender.com/"
             />
