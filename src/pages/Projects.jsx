48c2bdbd2a5cbcfb8a4024ddbccb1c1d694962f1
Diana Le <dianapnle@gmail.com>
2024-09-21 20:46:10 -0700

    updated resume and added meta data

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -23,7 +23,7 @@ const Projects = () => {
               imgPath={urbansteps}
               isBlog={false}
               title="UrbanSteps"
-              description="Urban Steps is a website and online directory that helps people discover and get involved in local dance studios in their area. Urban Steps' users can view dance studios, instructors,classes, and write reviews. Studio owners can add instructors to their studios and be able to have class schedules on here too! The backend of Spirited Get Away is built on SQL, sequelize, and express with a PostgreSQL database. Frontend rendering is handled with React and Redux."
+              description="Urban Steps is a website and online directory that helps people discover and get involved in local dance studios in their area. I have always been interested in combining my passion for dance and technical skills so I built this project to combine the two! A lot of dance studios in my area currently do not have their own websites, so this project's purpose was to resolve basic interactions a dance studio and student would want."
               ghLink="https://github.com/dianapnle/UrbanSteps"
               demoLink="https://urbansteps.onrender.com/"
             />
@@ -34,7 +34,7 @@ const Projects = () => {
               imgPath={spiritedaway}
               isBlog={false}
               title="Spirited Away"
-              description="Spirited Get Away is a clone of the website Airbnb, an online market place that connects people who are looking for accomodations in a particular area with people who are renting out their properties. The backend of Spirited Get Away is built on SQL, sequelize, and express with a PostgreSQL database. Frontend rendering is handled with React and Redux."
+              description="Spirited Get Away is a clone of the website Airbnb, an online market place that connects people who are looking for accomodations in a particular area with people who are renting out their properties. The backend of Spirited Get Away is built on Javascript, SQL, sequelize, and express with a PostgreSQL database. Frontend rendering is handled with React and Redux."
               ghLink="https://github.com/dianapnle/spirited-away"
               demoLink="https://spirited-away.onrender.com/"
             />
@@ -44,7 +44,7 @@ const Projects = () => {
               imgPath={xtalk}
               isBlog={false}
               title="X-Talk"
-              description="Cross-Talk is a clone of the website Discord, a free app that allows users to communicate in real time using text. Similarly, Cross-Talk allows for users to organize servers and smaller communities, rather than having a single central community. Users can join any public server and also create a smaller server for a group of friends.The backend of Cross-Talk is built on SQLAlchemy, Flask, python with a PostgreSQL database, AWS to handle media, and SocketIO for real-time messaging.  Frontend rendering is handled with React and Redux."
+              description="Cross-Talk is a Python-based clone of the website Discord, an app that allows users to communicate in real time using text. Similarly, Cross-Talk allows for users to organize servers and smaller communities, rather than having a single central community. Users can join any public server and also create a smaller server for a group of friends."
               ghLink="https://github.com/apatel978/Discord-Clone-XTalk"
               demoLink="https://xtalk-h48u.onrender.com/"
             />
